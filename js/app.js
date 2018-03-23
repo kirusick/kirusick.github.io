a3674c1c3b66bfda8bc81a384cd899b6906dd186
unknown <k.smolniy@gmail.com>
2018-03-24 02:28:58 +0300

    add localization, jobs, beatyLoading

--- js/app.js
+++ js/app.js
@@ -1,5 +1,41 @@
+const messages = {
+    en: {
+        'skills' : 'Skills:',
+        'fio' : 'Orlovskiy Kirill Vladimirovich',
+        'age' : 'Age: %{ages} years old',
+        'base_education': 'Education: BSUIR Affiliate "Minsk Radioengineering College", Software for Information Technology, Programmer (2013 - 2017)',
+        'hight_education': 'Education: BSUIR, Faculty of Computer Technologies, Software for Information Technology, Software-Engineer (2017 - 2021)',
+        'contacts_label': 'Contacts:',
+        'look_works': 'View works',
+        'work_href': 'View',
+        'page_title': 'Orlovskiy Kirill\'s portfolio',
+        'jobs_article': 'Worked/Work here:',
+        'in_progress': 'In progress',
+        'this_portfolio': 'This page'
+    },
+    ru: {
+        'skills' : 'Навыки:',
+        'fio' : 'Орловский Кирилл Владимирович',
+        'age' : 'Возраст: %{ages} лет',
+        'base_education': 'Образование: УО БГУИР "Филлиал МРК", ПОИТ, Техник-программист (2013 - 2017 гг)',
+        'hight_education': 'Образование: УО БГУИР, Факультет Компьютерных Технологий, ПОИТ, Инженер-программист (2017 - 2021 гг) (вечерняя форма обучения)',
+        'contacts_label': 'Контакты:',
+        'look_works': 'Посмотреть работы',
+        'work_href': 'Просмотреть',
+        'page_title': 'Портфолио Орловского Кирилла',
+        'jobs_article': 'Работал/Работаю там:',
+        'in_progress': 'В прогрессе',
+        'this_portfolio': 'Это портфолио'
+    }
+};
+
+const i18n = new VueI18n({
+    locale: 'en',
+    messages,
+});
+
 let app = new Vue({
-    el: '#app',
+    i18n,
     data: {
         skills: [
             'CSS',
@@ -11,8 +47,8 @@ let app = new Vue({
             'Nodejs',
             'JIRA',
             'jQuery',
-            'Адаптивная верстка',
-            'Photoshop для верстки',
+            'Responsive',
+            'Photoshop',
             'Vue',
             'Vuex',
             'Redux',
@@ -21,18 +57,39 @@ let app = new Vue({
         projects: [
             {
                 title: 'Bikeshop',
-                date: '9 Мая 2017',
+                date: '9 May 2017',
                 url:'layouts/bikeshop/index.html',
                 dateTime: '2017-05-09 20:00',
                 backgroundUrl: 'img/bikeshop.jpg'
             }, {
                 title: 'Glow',
                 dateTime: '2016-10-16 20:00',
-                date: '23 Октября 2016',
+                date: '23 October 2016',
                 url:'layouts/3/index.html',
                 backgroundUrl: 'img/glow.jpg'
+            }
+        ],
+        jobs: [
+            {
+                orgName: 'LWO',
+                from: 'September 2017',
+                to: 'January 2018',
+                month: 4,
+                logoUrl: 'https://img1.infobank.by/images/imagesindb/lwo_logo_180216.png.aspx',
+                position: 'Frontend Developer'
             },
-        ]
+            {
+                orgName: 'Ultalab',
+                from: 'January 2018',
+                to: 'Now',
+                logoUrl: 'https://companies.dev.by/system/companies/logos/67046/pre_medium_white/picture_67046.png',
+                month: (new Date()).getMonth() + ((new Date()).getFullYear() - 2018) * 11, 
+                position: 'Frontend Developer'
+            },
+        ],
+        isLoading: true,
+        avaLoaded: false,
+        lang: 'en'
     },
     computed: {
         ageOld : function() {
@@ -64,9 +121,26 @@ let app = new Vue({
             return age;
         }
     },
+    beforeMount: function() {
+        if (~window.location.search.indexOf('lang=')) {
+            let search = window.location.search;
+            this.lang = search.substr(search.indexOf('lang=')+5,2);
+        }
+        this.$i18n.locale = this.lang;
+    },
+    watch: {
+        lang: function(val) {
+            this.$i18n.locale = val;
+        }
+    },
+    mounted: function() {
+        this.$refs.visitCard.classList.remove('app-loading');
+        this.isLoading = false;
+        document.body.style.overflow = 'auto';
+    },
     methods: {
         scrollToWorks: function() {
             document.getElementById('works').scrollIntoView({behavior: 'smooth'});
         }
     }
-});
+}).$mount('#app');
